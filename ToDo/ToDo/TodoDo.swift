164f9d4f91e83d80f1903a1d435c6cf52d699fdc
Ryan <rdavid8@gmail.com>
2016-02-03 13:50:05 -0800

    startTODOapp

---
+++ ToDo/ToDo/TodoDo.swift
@@ -0,0 +1,44 @@
+//
+//  TodoDo.swift
+//  ToDo
+//
+//  Created by Ryan David on 2/3/16.
+//  Copyright © 2016 Michael Babiy. All rights reserved.
+//
+
+import Foundation
+
+protocol TodoDo: class
+{
+    typealias Object: Todo, NSCoding
+    
+    var items : [Object] { set get }
+    func add(object: Object)
+    func remove(object: Object)
+    func removeAllObjects()
+    func allObjects() -> [Object]
+    
+    
+}
+
+extension TodoDo
+{
+    func add(object: Object)
+    {
+        self.items.append(object)
+    }
+    func remove(object: Object)
+    {
+        self.items = self.items.filter({ (otherObject) -> Bool in
+            return object.identifier != otherObject.identifier
+        })
+}
+    func removeAllObjects()
+{
+    self.items.removeAll()
+    }
+    func allObjects() -> [Object]
+    {
+        return self.items
+    }
+}
